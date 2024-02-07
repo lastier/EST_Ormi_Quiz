4fb4ce4dafb8bbcdc505e410979acf7b4c39769f
HYUNSU <kwenhs0525@naver.com>
2024-02-08 02:02:06 +0900

    240207 Exception Example

--- src/dailyquiz240206/LoginExample.java
+++ src/dailyquiz240206/LoginExample.java
@@ -6,23 +6,25 @@ public class LoginExample {
             login("white", "12345");
         } catch(Exception e){
             System.out.println(e.getMessage());
+            e.printStackTrace();
         }
 
         try{
             login("blue", "54321");
         }catch(Exception e){
             System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 
     public static void login(String id, String password) throws NotExistsIdException, WrongPasswordException{
 
         if (!id.equals("blue")) {
-            throw new NotExistsIdException("아이디가 존재하지 않습니다.");
+            throw new NotExistsIdException(" 아이디가 존재하지 않습니다. "+id);
         }
 
         if (!password.equals("12345")) {
-            throw new WrongPasswordException("패스워드가 틀립니다.");
+            throw new WrongPasswordException(" 패스워드가 틀립니다. "+password);
         }
     }
 }
--- src/dailyquiz240206/NotExistsIdException.java
+++ src/dailyquiz240206/NotExistsIdException.java
@@ -2,7 +2,6 @@ package dailyquiz240206;
 
 public class NotExistsIdException extends Exception{
     public NotExistsIdException(){
-
     }
     public NotExistsIdException(String message){
         super(message);
